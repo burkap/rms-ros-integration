8db04a6b24cc750b21a0861327b805ff0d75abc0
burkap <brkpyzn@gmail.com>
2022-07-19 17:52:53 +0300

    Add wheel control and feedback features

--- rms_connect/scripts/wheel_controller.py
+++ rms_connect/scripts/wheel_controller.py
@@ -3,9 +3,15 @@ from std_msgs.msg import Float64MultiArray
 
 class WheelController:
     def on_command(self, data):
-        self.robo.chassis.drive_wheels(w1=data.data[0], w2=data.data[1], w3=data.data[2], w4=data.data[3])
+        self.data = data.data
+        print(self.data)
+        
+    def publish_message(self):
+        self.robo.chassis.drive_wheels(w1=self.data[0], w2=self.data[1], w3=self.data[2], w4=self.data[3])
 
     def __init__(self, robo):
         self.robo = robo
+        self.data = [0, 0, 0, 0]
         self.subscriber = rospy.Subscriber("/robomaster/control/wheel_speed", Float64MultiArray, self.on_command)
 
+    
--- rms_control/scripts/joy_control.py
+++ rms_control/scripts/joy_control.py
@@ -4,8 +4,7 @@ from sensor_msgs.msg import Joy
 from std_msgs.msg import Float64MultiArray
 
 class JoyControl:
-    def on_joy_command(self, data):
-        
+    def wheel_control(self, data):
         linear_x = data.axes[4]
         linear_y = data.axes[3]
 
@@ -17,25 +16,36 @@ class JoyControl:
 
         angular_z = data.axes[0]
 
-        w1 = (linear_x + angular_z + linear_y) / 2 * 500 * (1 + 0.5 * data.buttons[5])
-        w2 = (linear_x - angular_z - linear_y) / 2 * 500 * (1 + 0.5 * data.buttons[5])
-        w3 = (linear_x - angular_z + linear_y) / 2 * 500 * (1 + 0.5 * data.buttons[5])
-        w4 = (linear_x + angular_z - linear_y) / 2 * 500 * (1 + 0.5 * data.buttons[5])
+        w1 = (linear_x + angular_z + linear_y) / 2 * 200 * (1 + 0.5 * data.buttons[5])
+        w2 = (linear_x - angular_z - linear_y) / 2 * 200 * (1 + 0.5 * data.buttons[5])
+        w3 = (linear_x - angular_z + linear_y) / 2 * 200 * (1 + 0.5 * data.buttons[5])
+        w4 = (linear_x + angular_z - linear_y) / 2 * 200 * (1 + 0.5 * data.buttons[5])
         rospy.loginfo(f"{w1} {w2} {w3} {w4}")
         msg = Float64MultiArray()
         msg.data = [w1, w2, w3, w4]
-        self.publisher.publish(msg)
+        self.wheel_publisher.publish(msg)
+
+    def gimbal_control(self, data):
+        yaw = - data.axes[6]
+        pitch = data.axes[7]
+        
+        msg = Float64MultiArray()
+        msg.data = [pitch, yaw]
+        self.gimbal_publisher.publish(msg)
+    
+    def on_joy_command(self, data):
+        self.wheel_control(data)
+        self.gimbal_control(data)
 
     def __init__(self):
         rospy.init_node('rms_joy_control', anonymous=True)
-        
         self.sub = rospy.Subscriber("/joy", Joy, self.on_joy_command)
-        self.publisher = rospy.Publisher("/robomaster/control/wheel_speed", Float64MultiArray, queue_size=10)
-
-        self.rate = rospy.Rate(10)
+        self.wheel_publisher = rospy.Publisher("/robomaster/control/wheel_speed", Float64MultiArray, queue_size=10)
+        self.gimbal_publisher = rospy.Publisher("/robomaster/control/gimbal", Float64MultiArray, queue_size=10)
+        self.gimbal_coeff = 10
+        self.rate = rospy.Rate(30)
            
         while (not rospy.is_shutdown()):
-
             self.rate.sleep()
               
 
