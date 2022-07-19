490edf4656598f8c495cca3fbc3d54eb522e645d
burkap <brkpyzn@gmail.com>
2022-07-19 17:56:03 +0300

    Publish position information as a ROS message

--- rms_connect/scripts/position_handler.py
+++ rms_connect/scripts/position_handler.py
@@ -2,22 +2,26 @@ import rospy
 from tf.transformations import quaternion_from_euler
 import tf
 from imu_handler import IMUHandler
+from std_msgs.msg import Float64MultiArray
 
 
 class PositionHandler:
     def __init__(self, IMUHandler):
         self.broadcaster = tf.TransformBroadcaster()
+        self.float64_pub = rospy.Publisher("/robomaster/feedback/position_info", Float64MultiArray, queue_size=10)
         self.imu_handler = IMUHandler
     
     def position_info_handler(self, info):
         x, y, z = info
-        # rospy.loginfo("chassis position: x:{0}, y:{1}, z:{2}".format(x, y, z))
         
+        msg = Float64MultiArray()
+        msg.data = [x, y, z]
+        self.float64_pub.publish(msg)
         quat = quaternion_from_euler(self.imu_handler.roll, self.imu_handler.pitch, self.imu_handler.yaw)
         
         self.broadcaster.sendTransform((x,y,0),
                                        quat,
                                        rospy.Time.now(),
-                                       "map",
-                                       "base_link")
+                                       "base_link",
+                                       "map")
     
